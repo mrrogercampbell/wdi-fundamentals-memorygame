880ada88555a2fd22f67be4cd312dfbf81ca5bae
Roger Campbell II <roger@Rogers-MBP.home>
2018-11-06 11:39:28 -0500

    Added game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,22 @@
-var cardOne = "king";
-var	cardTwo	= "king";
-var cardThree = "queen";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne); {
+	console.log("User flipped Queen");
+}
+
+cardsInPlay.push(cardTwo); {
+	console.log("User flipped King");
+}
+
+if  (cardsInPlay.length === 2) {
+} if (cardsInPlay === cards[0] && cardsInPlay === cards[1]){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+} 
